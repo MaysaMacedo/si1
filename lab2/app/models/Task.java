562ee9e9ed048e015f4e06e44d195848d0ad1e70
MaysaMacedo <maysa.souza@ccc.ufcg.edu.br>
2013-11-24 16:05:30 -0200

    Atualizando

---
+++ lab2/app/models/Task.java
@@ -0,0 +1,24 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import play.data.validation.Constraints.*;
+
+public class Task {
+
+	public Long id;
+	@Required
+	public String label;
+
+	public static List<Task> all() {
+		return new ArrayList<Task>();
+	}
+
+	public static void create(Task task) {
+	}
+
+	public static void delete(Long id) {
+	}
+
+}
