dcdf52087d09764bfa7e6265028d889be17ea3e0
MaysaMacedo <maysa.souza@ccc.ufcg.edu.br>
2013-11-24 15:15:58 -0200

    Atualizando

--- lab2/app/controllers/Application.java
+++ lab2/app/controllers/Application.java
@@ -7,11 +7,20 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-    
-    public static Result helloWorld() {
-		return ok("Hello World");
+	public static Result index() {
+		return redirect(routes.Application.tasks());
 	}
+
+	public static Result tasks() {
+		return TODO;
+	}
+
+	public static Result newTask() {
+		return TODO;
+	}
+
+	public static Result deleteTask(Long id) {
+		return TODO;
+	}
+
 }
