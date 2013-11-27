50b476f0a70cf47bd9a2298b845bad7198c8f232
MaysaMacedo <maysa.souza@ccc.ufcg.edu.br>
2013-11-26 22:51:22 -0200

    Last version

--- lab2/app/controllers/Application.java
+++ lab2/app/controllers/Application.java
@@ -10,14 +10,25 @@ import play.mvc.Result;
 public class Application extends Controller {
 	static Form<Task> taskForm = Form.form(Task.class);
 
+	/**
+	 * Ação para a rota
+	**/
 	public static Result index() {
 		return redirect(routes.Application.tasks());
 	}
 
+	/**
+	 * Ação da página inicial
+	 * @return
+	 */
 	public static Result tasks() {
 		return ok(views.html.index.render(Task.all(), taskForm));
 	}
-
+	
+	/**
+	 * Ação de cadastro de novas atividades
+	 * @return
+	 */
 	public static Result newTask() {
 		Form<Task> filledForm = taskForm.bindFromRequest();
 		if (filledForm.hasErrors()) {
@@ -28,12 +39,21 @@ public class Application extends Controller {
 		}
 	}
 
+	/**
+	 * Ação de remover uma determinada atividade
+	 * @param id
+	 * @return
+	 */
 	public static Result deleteTask(Long id) {
 		  Task.delete(id);
 		  return redirect(routes.Application.tasks());
 		}
 	
-	
+	/**
+	 * Ação de atualizar o status da atividade
+	 * @param id
+	 * @return
+	 */
 	public static Result updateTask(Long id) {
 		Task.updateStatus(id);
 		return redirect(routes.Application.tasks());
